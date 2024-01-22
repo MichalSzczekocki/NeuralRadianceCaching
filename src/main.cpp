93b02f51a6d09fa265d1b9d279d56583fe985214
MichaelS <michal.szczekocki.poznan@gmail.com>
2024-01-23 00:45:58 +0100

    Clear old pipeline from nrchpmrenderer and add render.comp

--- src/main.cpp
+++ src/main.cpp
@@ -101,7 +101,7 @@ void SwapchainResizeCallback()
 
     uint32_t width = en::Window::GetWidth();
     uint32_t height = en::Window::GetHeight();
-    nrcHpmRenderer->ResizeFrame(width, height);
+//    nrcHpmRenderer->ResizeFrame(width, height);
     en::ImGuiRenderer::Resize(width, height);
     en::ImGuiRenderer::SetBackgroundImageView(nrcHpmRenderer->GetImageView());
 }
@@ -165,8 +165,8 @@ void RunNrcHpm()
             camera,
             volumeData,
             dirLight, pointLight, hdrEnvMap,
-            nrc,
-            mrhe);
+            nrc);
+//            mrhe);
 
     en::ImGuiRenderer::Init(width, height);
     en::ImGuiRenderer::SetBackgroundImageView(nrcHpmRenderer->GetImageView());
